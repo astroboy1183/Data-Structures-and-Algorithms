9bb919e7eb092c58a86901e6ae031f89062e2264
astroboy1183 <jayanthapalla@gmail.com>
2026-06-08 22:42:02 +0530

    Arrays in Python.

---
+++ 01_Arrays/01_arrays.py
@@ -0,0 +1,29 @@
+import array
+import numpy as np
+
+#creation of array
+my_array = array.array('i')
+
+print(my_array)
+
+my_array1 = array.array('i', [1, 2, 3, 4, 5])
+print(my_array1)
+
+my_array2 = np.array([1, 2, 3, 4, 5])
+print(my_array2)
+
+my_array3 = np.array([], dtype=int)
+print(my_array3)
+
+#inserting elements in array
+my_array1.insert(0, 0)
+my_array1.insert(3,45)
+my_array1.insert(100,88)
+print(my_array1)
+
+#array traversal
+# for i in my_array1:
+#     print(i)
+
+for i in enumerate(my_array1):
+    print(i[1])
---
+++ 01_Arrays/02_arrays.py
@@ -0,0 +1,35 @@
+import array
+
+my_array = array.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+def accessElement(array, index):
+    if index <= len(array):
+        return array[index]
+    else:
+        return "Index out of bounds"
+
+#linear search
+def linearSearch(array, target):
+    for i in range(len(array)):
+        if array[i] == target:
+            return i
+    return "Element not found"
+
+#binary search
+def binarySearch(array, target):
+    left, right = 0, len(array) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return "Element not found"
+
+print(accessElement(my_array, 2))
+print(accessElement(my_array, 10))
+print(linearSearch(my_array, 3))
+print(linearSearch(my_array, 8))
+print(binarySearch(my_array, 3))
+print(binarySearch(my_array, 7))
